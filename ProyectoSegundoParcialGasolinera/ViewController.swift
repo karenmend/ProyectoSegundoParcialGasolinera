1fd4631366f0aca0bfa55d76bfc76e4b8238c7f9
Alumno <ulsa@iMacD109.local>
2019-10-08 14:46:26 -0700

    Primer commit

---
+++ ProyectoSegundoParcialGasolinera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProyectoSegundoParcialGasolinera
+//
+//  Created by Alumno on 10/8/19.
+//  Copyright © 2019 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
